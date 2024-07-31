835d031938ba5305b86c670422c84b0a7379298c
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-07-31 23:54:43 +0900

    [Silver I] Title: 수학 퀴즈, Time: 208 ms, Memory: 4400 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/27467. 수학 퀴즈/수학 퀴즈.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define MOD 998244353LL
+#define INF 987654321987654321LL
+#define v(x) x.begin(),x.end()
+#define fi(x) for(i=0;i<x;i++)
+#define fi1(x) for(i=1;i<=x;i++)
+#define fj(x) for(j=0;j<x;j++)
+#define fj1(x) for(j=1;j<=x;j++)
+#define fk(x) for(k=0;k<x;k++)
+#define fk1(x) for(k=1;k<=x;k++)
+#define fastio() {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define pq priority_queue
+#define vvi vector<vector<int> >
+#define X first
+#define Y second
+#define endl '\n'
+#define int long long
+
+using namespace std;
+typedef pair<int, int> P;
+
+int n;
+int a[300005];
+
+signed main()
+{
+    int i,j;
+    int p = 0, q = 0;
+
+    cin>>n;
+    fi1(n) {
+        cin>>a[i];
+
+        if (a[i]%3 == 2) {
+            p--;
+            q--;
+        } else if (a[i]%3 == 0) {
+            q++;
+        } else {
+            p++;
+        }
+    }
+    cout<<p<<" "<<q<<endl;
+}
