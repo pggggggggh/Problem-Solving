5804553365a1a4877b3f7599012a5d8dab801232
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-08-27 19:23:33 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -3,31 +3,27 @@
 #define int long long
 using namespace std;
 
-int par[105];
-int sz[105];
+int vis[105];
+vector<int> adj[105];
 
-int find(int x) {
-    if (par[x] == x) return x;
-    return par[x] = find(par[x]);
-}
-
-void uni(int x, int y) {
-    x = find(x);
-    y = find(y);
-    if (x == y) return;
-    if (y == 1) swap(x, y);
-    sz[x] += sz[y];
-    par[y] = x;
+int dfs(int v) {
+    int res = 1;
+    vis[v] = 1;
+    for (auto &u: adj[v]) {
+        if (vis[u]) continue;
+        res += dfs(u);
+    }
+    return res;
 }
 
 signed main() {
     int n, k;
     cin >> n >> k;
-    for (int i = 1; i <= n; i++) par[i] = i, sz[i] = 1;
     for (int i = 0; i < k; i++) {
         int x, y;
         cin >> x >> y;
-        uni(x, y);
+        adj[x].push_back(y);
+        adj[y].push_back(x);
     }
-    cout << sz[1] - 1;
+    cout << dfs(1) - 1;
 }
