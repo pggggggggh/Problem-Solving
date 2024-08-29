0e3c9a7e5bc3f2cb9a584bbcb0cc596c52ceb54e
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-08-30 02:05:11 +0900

    [Platinum III] Title: 뒤집기 K, Time: 248 ms, Memory: 27048 KB -BaekjoonHub

---
+++ 백준/Platinum/21162. 뒤집기 K/뒤집기 K.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define all(x) begin(x), end(x)
+#define int long long
+using namespace std;
+using pi = pair<int,int>;
+
+struct suffix_array {
+    vector<int> sa, lcp;
+    // both include empty prefix at index 0, lcp[x] = lcp of sa[x], sa[x-1]
+
+    suffix_array(basic_string<int> &s, int lim = 200001) {
+        int n = s.size() + 1, k = 0, a, b;
+        vector<int> x(n), y(n), ws(max(n, lim)), rank(n);
+        for (int i = 0; i < n; i++) x[i] = s[i];
+        sa = lcp = y, iota(all(sa), 0);
+        for (int j = 0, p = 0; p < n; j = max(1LL, j * 2), lim = p) {
+            p = j, iota(all(y), n - j);
+            for (int i = 0; i < n; i++) if (sa[i] >= j) y[p++] = sa[i] - j;
+            fill(all(ws), 0);
+            for (int i = 0; i < n; i++) ws[x[i]]++;
+            for (int i = 1; i < lim; i++) ws[i] += ws[i - 1];
+            for (int i = n; i--;) sa[--ws[x[y[i]]]] = y[i];
+            swap(x, y), p = 1, x[sa[0]] = 0;
+            for (int i = 1; i < n; i++)
+                a = sa[i - 1], b = sa[i], x[b] =
+                        (y[a] == y[b] && y[a + j] == y[b + j]) ? p - 1 : p++;
+        }
+        for (int i = 1; i < n; i++) rank[sa[i]] = i;
+        for (int i = 0, j; i < n - 1; lcp[rank[i++]] = k)
+            for (k && k--, j = sa[rank[i] - 1];
+                 s[i + k] == s[j + k]; k++);
+    }
+};
+
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    reverse(all(a));
+    a.insert(a.end(),all(a));
+    basic_string ba(all(a));
+    suffix_array _sa(ba);
+    auto sa = _sa.sa;
+    for (int i = 1; i <= 2 * n; i++) {
+        if (sa[i] && sa[i] < n && !--k) {
+            for (int j = sa[i]; j < sa[i] + n; j++) cout << a[j] << ' ';
+            return 0;
+        }
+    }
+}
