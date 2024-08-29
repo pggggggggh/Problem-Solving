72867af25655fb7f20c774e6c802416bd9e45db6
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-08-29 21:28:47 +0900

    [Platinum IV] Title: 구간 합 구하기 2, Time: 172 ms, Memory: 42616 KB -BaekjoonHub

--- 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
+++ 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
@@ -5,105 +5,115 @@ using namespace std;
 using pi = pair<int,int>;
 
 struct lazyseg {
-    static constexpr int e = 0;
+    using S = int;
 
-    static int op(int a, int b) {
+    using L = int;
+
+    static constexpr S seg_e = 0; // identity of seg_op
+
+    static constexpr L lazy_e = 0; // identity of lazy_op
+
+    static S seg_op(S a, S b) { // combine two nodes
         return a + b;
     }
 
-    static int comp(int a, int b) { // b times
-        return a * b;
+    static L lazy_op(L a, L b) { // combine two lazy values
+        return a + b;
     }
 
-    int sz;
-    vector<int> seg;
-    vector<int> lazy;
+    static void apply_lazy(S &s, L l, int len) {
+        s += l * len;
+    }
 
-    lazyseg(int n) {
+    int n, sz;
+    vector<S> seg;
+    vector<L> lazy;
+
+    lazyseg(int n, const vector<S> &v = vector<S>()) {
+        assert(n >= v.size());
+        this->n = n;
         sz = 1;
         while (sz < n) sz *= 2;
-        seg = vector(2 * sz, e);
-        lazy = vector(2 * sz, e);
+        seg = vector(2 * sz, seg_e);
+        lazy = vector(2 * sz, lazy_e);
+        for (int i = 0; i < v.size(); i++) seg[i + sz] = v[i];
+        for (int i = sz - 1; i >= 1; i--) seg[i] = seg_op(seg[i * 2], seg[i * 2 + 1]);
     }
 
     void propa(int node, int l, int r) {
-        if (lazy[node] == 0) return;
-        seg[node] = op(seg[node], comp(lazy[node], r - l + 1));
+        if (lazy[node] == lazy_e) return;
+        apply_lazy(seg[node], lazy[node], r - l + 1);
         if (l != r) {
-            lazy[node * 2] = op(lazy[node * 2], lazy[node]);
-            lazy[node * 2 + 1] = op(lazy[node * 2 + 1], lazy[node]);
+            lazy[node * 2] = lazy_op(lazy[node * 2], lazy[node]);
+            lazy[node * 2 + 1] = lazy_op(lazy[node * 2 + 1], lazy[node]);
         }
-        lazy[node] = e;
+        lazy[node] = lazy_e;
     }
 
-    void set(int x, int val, int node, int l, int r) {
+    void put(int x, S val, int node, int l, int r) {
         propa(node, l, r);
         if (l == r) {
             seg[node] = val;
             return;
         }
         int mid = l + r >> 1;
-        if (x <= mid) set(x, val, node * 2, l, mid);
-        else set(x, val, node * 2 + 1, mid + 1, r);
-        seg[node] = op(seg[node * 2], seg[node * 2 + 1]);
+        if (x <= mid) put(x, val, node * 2, l, mid);
+        else put(x, val, node * 2 + 1, mid + 1, r);
+        seg[node] = seg_op(seg[node * 2], seg[node * 2 + 1]);
     }
 
-    void set(int x, int val) {
-        set(x, val, 1, 0, sz - 1);
+    void put(int x, S val) {
+        put(x, val, 1, 0, sz - 1);
     }
 
-    void apply(int x, int y, int val, int node, int l, int r) {
+    void update(int x, int y, S val, int node, int l, int r) {
         propa(node, l, r);
         if (r < x || y < l) return;
         if (x <= l && r <= y) {
-            lazy[node] = op(lazy[node], val);
+            lazy[node] = lazy_op(lazy[node], val);
             propa(node, l, r);
             return;
         }
         int mid = l + r >> 1;
-        apply(x, y, val, node * 2, l, mid);
-        apply(x, y, val, node * 2 + 1, mid + 1, r);
-        seg[node] = op(seg[node * 2], seg[node * 2 + 1]);
+        update(x, y, val, node * 2, l, mid);
+        update(x, y, val, node * 2 + 1, mid + 1, r);
+        seg[node] = seg_op(seg[node * 2], seg[node * 2 + 1]);
     }
 
-    void apply(int x, int y, int val) {
-        apply(x, y, val, 1, 0, sz - 1);
+    void update(int x, int y, S val) {
+        update(x, y, val, 1, 0, sz - 1);
     }
 
-    void apply(int x, int val) {
-        apply(x, x, val);
-    }
-
-    int query(int x, int y, int node, int l, int r) {
+    S query(int x, int y, int node, int l, int r) {
         propa(node, l, r);
-        if (r < x || y < l) return e;
-        if (x <= l && r <= y)return seg[node];
+        if (r < x || y < l) return seg_e;
+        if (x <= l && r <= y) return seg[node];
         int mid = l + r >> 1;
-        return op(query(x, y, node * 2, l, mid), query(x, y, node * 2 + 1, mid + 1, r));
+        return seg_op(query(x, y, node * 2, l, mid), query(x, y, node * 2 + 1, mid + 1, r));
     }
 
-    int query(int x, int y) {
+    S query(int x, int y) {
         return query(x, y, 1, 0, sz - 1);
     }
 };
+
 
 signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int n, m, k;
     cin >> n >> m >> k;
-    lazyseg seg(n + 1);
+    vector<int> a(n + 1);
     for (int i = 1; i <= n; i++) {
-        int x;
-        cin >> x;
-        seg.set(i, x);
+        cin >> a[i];
     }
+    lazyseg seg(n + 1, a);
     for (int i = 0; i < m + k; i++) {
         int w, x, y, z;
         cin >> w >> x >> y;
         if (w == 1) {
             cin >> z;
-            seg.apply(x, y, z);
+            seg.update(x, y, z);
         } else cout << seg.query(x, y) << '\n';
     }
 }
