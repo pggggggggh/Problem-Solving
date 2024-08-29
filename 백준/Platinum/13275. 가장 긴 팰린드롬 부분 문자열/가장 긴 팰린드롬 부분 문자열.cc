389e2c2657aa2826f57d891c3fb4a06119488e12
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-08-30 02:59:02 +0900

    [Platinum V] Title: 가장 긴 팰린드롬 부분 문자열, Time: 48 ms, Memory: 12048 KB -BaekjoonHub

---
+++ 백준/Platinum/13275. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define all(x) begin(x), end(x)
+#define int long long
+using namespace std;
+using pi = pair<int,int>;
+
+struct suffix_array {
+    vector<int> sa, lcp;
+
+    suffix_array(string &s, int lim = 256) { // or vector<int>, whatever.. lim = max_element+1
+        int n = s.size() + 1, k = 0, a, b;
+        vector<int> x(n), y(n), ws(max(n, lim)), rank(n);
+        for (int i = 0; i < n - 1; i++) x[i] = s[i];
+        sa = lcp = y, iota(all(sa), 0);
+        for (int j = 0, p = 0; p < n; j = max(1LL, j * 2), lim = p) {
+            p = j, iota(all(y), n - j);
+            for (int i = 0; i < n; i++) if (sa[i] >= j) y[p++] = sa[i] - j;
+            fill(all(ws), 0);
+            for (int i = 0; i < n; i++) ws[x[i]]++;
+            for (int i = 1; i < lim; i++) ws[i] += ws[i - 1];
+            for (int i = n; i--;) sa[--ws[x[y[i]]]] = y[i];
+            swap(x, y), p = 1, x[sa[0]] = 0;
+            for (int i = 1; i < n; i++)
+                a = sa[i - 1], b = sa[i], x[b] =
+                        (y[a] == y[b] && y[a + j] == y[b + j]) ? p - 1 : p++;
+        }
+        for (int i = 1; i < n; i++) rank[sa[i]] = i;
+        for (int i = 0, j; i < n - 1; lcp[rank[i++]] = k)
+            for (k && k--, j = sa[rank[i] - 1];
+                 s[i + k] == s[j + k]; k++);
+    }
+};
+
+signed main() {
+    string s;
+    cin >> s;
+    s = s + '$' + string(s.rbegin(), s.rend());
+    suffix_array _s(s);
+    auto sa = _s.sa;
+    auto lcp = _s.lcp;
+    int res = 0;
+    int half = (s.size() - 1) / 2;
+    for (int i = 1; i <= s.size(); i++) {
+        if (sa[i - 1] < half != sa[i] < half) res = max(res, lcp[i]);
+    }
+    cout << res;
+}
