3960bf49f7776455c9a7d1135cf4d115710fa62e
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-09-01 01:42:18 +0900

    [Platinum V] Title: 가장 긴 팰린드롬 부분 문자열, Time: 4 ms, Memory: 3836 KB -BaekjoonHub

---
+++ 백준/Platinum/14444. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define all(x) begin(x), end(x)
+#define int long long
+using namespace std;
+using pi = pair<int,int>;
+
+array<vector<int>, 2> manacher(const string &s) {
+    /* max even palindrome around i : p[0][i] * 2
+     * max odd palindrome around i : p[1][i] * 2 + 1 */
+    int n = s.size();
+    array p = {vector<int>(n + 1), vector<int>(n)};
+    for (int z = 0; z < 2; z++) {
+        for (int i = 0, l = 0, r = 0; i < n; i++) {
+            int t = r - i + !z;
+            if (i < r) p[z][i] = min(t, p[z][l + t]);
+            int L = i - p[z][i], R = i + p[z][i] - !z;
+            while (L >= 1 && R + 1 < n && s[L - 1] == s[R + 1])
+                p[z][i]++, L--, R++;
+            if (R > r) l = L, r = R;
+        }
+    }
+    return p;
+}
+
+signed main() {
+    string s;
+    cin >> s;
+    auto p = manacher(s);
+    int res = 0;
+    for (int i = 0; i < s.size(); i++) res = max(res, max(p[0][i] * 2, p[1][i] * 2 + 1));
+    cout << res;
+}
