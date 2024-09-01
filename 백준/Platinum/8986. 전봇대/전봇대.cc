79b811d4eed93ca00f34232f6134f27ade361df7
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-09-01 23:23:11 +0900

    [Platinum V] Title: 전봇대, Time: 0 ms, Memory: 2020 KB, Score: 32 point -BaekjoonHub

---
+++ 백준/Platinum/8986. 전봇대/전봇대.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define all(x) begin(x), end(x)
+#define int long long
+using namespace std;
+using pi = pair<int,int>;
+
+int n;
+vector<int> a;
+
+int f(int x) {
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        res += abs(i * x - a[i]);
+    }
+    return res;
+}
+
+signed main() {
+    cin >> n;
+    a.resize(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    int lo = 0, hi = 1e9;
+    while (lo + 3 < hi) {
+        int p = (2 * lo + hi) / 3;
+        int q = (lo + 2 * hi) / 3;
+        if (f(p) < f(q)) hi = q;
+        else lo = p;
+    }
+    int res = 1e9;
+    for (int i = lo; i <= hi; i++) {
+        res = min(res, f(i));
+    }
+    cout << res;
+}
