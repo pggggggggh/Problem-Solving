c2628d2e5792caf02eddba0ab3863514405bacdb
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-09-02 00:03:58 +0900

    [Silver II] Title: 랜선 자르기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define all(x) begin(x), end(x)
+#define int long long
+using namespace std;
+using pi = pair<int,int>;
+
+signed main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    int lo = 0, hi = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        hi = max(hi, a[i] + 1);
+    }
+    function<int(int)> able = [&](int x) {
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] < x) continue;
+            res += a[i] / x;
+        }
+        return res >= m;
+    };
+    while (lo + 1 < hi) {
+        int mid = lo + hi >> 1;
+        if (able(mid)) lo = mid;
+        else hi = mid;
+    }
+    cout << lo;
+}
