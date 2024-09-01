f241bc6f87a19febf670a1db5d16ef5796742529
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-09-01 23:42:00 +0900

    [Platinum V] Title: 전봇대, Time: 8 ms, Memory: 2020 KB, Score: 63 point -BaekjoonHub

--- 백준/Platinum/8986. 전봇대/전봇대.cc
+++ 백준/Platinum/8986. 전봇대/전봇대.cc
@@ -20,15 +20,15 @@ signed main() {
     a.resize(n);
     for (int i = 0; i < n; i++) cin >> a[i];
 
-    int lo = 0, hi = 1e9;
-    while (lo + 3 < hi) {
+    int lo = 0, hi = 1e10;
+    while (lo + 5 <= hi) {
         int p = (2 * lo + hi) / 3;
         int q = (lo + 2 * hi) / 3;
         if (f(p) < f(q)) hi = q;
         else lo = p;
     }
-    int res = 1e9;
-    for (int i = lo; i <= hi; i++) {
+    int res = f(lo);
+    for (int i = lo + 1; i <= hi; i++) {
         res = min(res, f(i));
     }
     cout << res;
