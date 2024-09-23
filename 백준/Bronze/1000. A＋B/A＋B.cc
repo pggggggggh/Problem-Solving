868ecac326717bfe6c29ea0baf4fa9cfe88e4cea
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-09-23 20:27:50 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define all(v) (v).begin(), (v).end()
+#define int long long
+using namespace std;
+using pi = pair<int, int>;
+
+signed main() {
+  int a, b;
+  cin >> a >> b;
+  cout << a + b;
+}
