fbc434cdf32581b4c17b1e1392b3457e26139982
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-10-09 14:27:16 +0900

    [Bronze II] Title: Eszett, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28373. Eszett/Eszett.py
@@ -0,0 +1,10 @@
+s = input().lower()
+if 'sss' in s:
+    print(s)
+    print(s.replace('sss', 'sB'))
+    print(s.replace('sss', 'Bs'))
+elif 'ss' in s:
+    print(s)
+    print(s.replace('ss', 'B'))
+else:
+    print(s)
