6346f034c242bf1eac733f75f622add12ba53b6f
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-10-12 15:10:32 +0900

    [Platinum IV] Title: 마법 구슬, Time: 140 ms, Memory: 17652 KB, Score: 17 point -BaekjoonHub

--- 백준/Platinum/32129. 마법 구슬/마법 구슬.cc
+++ 백준/Platinum/32129. 마법 구슬/마법 구슬.cc
@@ -1,35 +1,57 @@
 #include <bits/stdc++.h>
+#define all(v) (v).begin(), (v).end()
 #define int long long
-#define all(x) (x).begin(),(x).end()
 using namespace std;
 using pi = pair<int, int>;
 
-int n, q;
-int a[500005];
-
-void roll() {
-    for (int i = 1; i <= n; i++) {
-        if (a[i] <= a[i + 1]) {
-            a[i]++;
-            return;
-        }
-    }
+void solve()
+{
+	int n, q;
+	cin >> n >> q;
+	vector<int> a(n + 1), needed(n + 1), heights(n + 1);
+	vector<int> naive(n + 1);
+	int sum = 0;
+	int stair = 0;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+		naive[i] = a[i];
+		stair = max(stair - 1, a[i]);
+		sum += a[i];
+		needed[i] = (stair + stair + i - 1) * i / 2 - sum;
+		heights[i] = stair + i - 1;
+		// cout << needed[i] << ' ' << heights[i] << '\n';
+	}
+	int cur = 0;
+	while (q--) {
+		int x, y;
+		cin >> x;
+		if (x == 1) {
+			cin >> y;
+			cur += y;
+			while (y--) {
+				int idx = 1;
+				while (idx < n && naive[idx] > naive[idx + 1]) idx++;
+				naive[idx]++;
+			}
+		} else {
+			int idx = prev(upper_bound(all(needed), cur)) - needed.begin();
+			int orig_height = heights[idx];
+			if (idx != n && heights[idx] == heights[idx + 1])
+				cout << orig_height << '\n';
+			else
+				cout << orig_height + (cur - needed[idx]) / idx << '\n';
+			// for (int i = 1; i <= n; i++) cout << naive[i] << ' ';
+			// cout << " : " << cur;
+			// cout << '\n';
+		}
+	}
 }
 
-signed main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n >> q;
-    for (int i = 1; i <= n; i++) cin >> a[i];
-    a[n + 1] = 1e18;
-    while (q--) {
-        int x, y;
-        cin >> x;
-        if (x == 1) {
-            cin >> y;
-            while (y--) roll();
-        } else {
-            cout << a[1] << '\n';
-        }
-    }
+signed main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	solve();
 }
