2e66ff68874a4843a7b591fb3a39ffd9cc20dcd0
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-10-30 02:54:15 +0900

    [Gold V] Title: 2배 또는 0.5배, Time: 160 ms, Memory: 17648 KB, Score: 33 point -BaekjoonHub

---
+++ 백준/Gold/27468. 2배 또는 0．5배/2배 또는 0．5배.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define all(v) (v).begin(), (v).end()
+#define int long long
+using namespace std;
+using pi = pair<int, int>;
+
+void solve()
+{
+	int n;
+	cin >> n;
+	vector<int> vis(n + 1);
+	vis[0] = 1;
+	int cur = 1;
+	if (n % 2 == 0) cur = 2;
+	vis[cur] = 1;
+	cout << "YES\n";
+	cout << cur << ' ';
+	while (cur <= n || !vis[n - 1]) {
+		if (!vis[cur - 1]) {
+			cur--;
+			vis[cur] = 1;
+			cout << cur << ' ';
+			if (cur + 2 > n) break;
+			cur += 2;
+			vis[cur] = 1;
+			cout << cur << ' ';
+		} else {
+			cur++;
+			vis[cur] = 1;
+			cout << cur << ' ';
+			if (cur == n && vis[n - 1]) break;
+			cur += 2;
+			vis[cur] = 1;
+			cout << cur << ' ';
+		}
+	}
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	solve();
+}
