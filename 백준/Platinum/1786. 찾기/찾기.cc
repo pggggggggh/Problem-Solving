ec3e4c8f95c928c39d5cf6db73b3f23c6addd5ed
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-11-05 20:43:26 +0900

    [Platinum V] Title: 찾기, Time: 60 ms, Memory: 22272 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.cc
+++ 백준/Platinum/1786. 찾기/찾기.cc
@@ -1,58 +1,38 @@
 #include <bits/stdc++.h>
 #define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
 #define int long long
 using namespace std;
-using pi = pair<int,int>;
+using pi = pair<int, int>;
 
-namespace kmp {
-    static vector<int> failure_func(const string &s) {
-        // the length of the longest matching prefix and suffix
-        vector<int> fail(s.size());
-        for (int i = 1, j = 0; i < s.size(); i++) {
-            while (j > 0 && s[i] != s[j]) j = fail[j - 1];
-            if (s[i] == s[j]) fail[i] = ++j;
-        }
-        return fail;
-    }
+void solve()
+{
+	string a, b;
+	getline(cin, a);
+	getline(cin, b);
+	int n = a.size(), m = b.size();
+	vector<int> fail(m);
+	for (int i = 1, j = 0; i < m; i++) {
+		while (j > 0 && b[i] != b[j]) j = fail[j - 1];
+		if (b[i] == b[j]) fail[i] = ++j;
+	}
+	vector<int> match(n);
+	for (int i = 0, j = 0; i < n; i++) {
+		while (j > 0 && a[i] != b[j]) j = fail[j - 1];
+		if (a[i] == b[j]) match[i] = ++j;
+	}
+	vector<int> ans;
+	for (int i = 0; i < n; i++) {
+		if (match[i] == m) ans.push_back(i);
+	}
+	cout << ans.size() << '\n';
+	for (auto& x : ans) cout << x - m + 2 << ' ';
+}
 
-    static vector<int> match(const string &a, const string &b) {
-        vector<int> res;
-        vector<int> fail = failure_func(b);
-        for (int i = 0, j = 0; i < a.size(); i++) {
-            while (j > 0 && a[i] != b[j]) j = fail[j - 1];
-            if (a[i] == b[j]) {
-                if (j == b.size() - 1) {
-                    res.push_back(i - b.size() + 1 + 1);
-                    j = fail[j];
-                } else j++;
-            }
-        }
-        return res;
-    }
+signed main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 
-    static vector<int> z_func(const string &s) {
-        vector<int> z(s.size());
-        z[0] = s.size();
-        int l = 0, r = 0; // [l,r)
-        for (int i = 1; i < s.size(); i++) {
-            if (i < r) z[i] = min(r - i, z[i - l]);
-            while (i + z[i] < s.size() && s[z[i]] == s[i + z[i]]) {
-                z[i]++;
-            }
-            if (i + z[i] > r) {
-                l = i;
-                r = i + z[i];
-            }
-        }
-        return z;
-    }
-};
-
-signed main() {
-    string a, b;
-    getline(cin, a);
-    getline(cin, b);
-    auto res = kmp::match(a, b);
-    cout << res.size() << '\n';
-    for (auto &x: res) cout << x << '\n';
+	solve();
 }
