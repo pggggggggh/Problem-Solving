334f638545370e592b761c7132ea9f8fc273c9c2
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-11-16 01:32:51 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define int long long
+using namespace std;
+using pi = pair<int, int>;
+
+void solve()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a * b;
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	solve();
+}
