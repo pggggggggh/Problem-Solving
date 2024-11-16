a1ac5a6248e4d7ae4c6e086a285200c69f01b4aa
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-11-16 14:53:50 +0900

    [Bronze V] Title: Just Round Down, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define int long long
+using namespace std;
+using pi = pair<int, int>;
+
+void solve()
+{
+	double d;
+	cin >> d;
+	cout << (int)d;
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	solve();
+}
