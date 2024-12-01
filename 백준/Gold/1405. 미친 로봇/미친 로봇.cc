70765d2980eaad85c768c42d7cc84d5ea7c948e2
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-12-01 19:23:58 +0900

    [Gold IV] Title: 미친 로봇, Time: 20 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1405. 미친 로봇/미친 로봇.cc
+++ 백준/Gold/1405. 미친 로봇/미친 로봇.cc
@@ -4,7 +4,7 @@
 using namespace std;
 using pi = pair<int, int>;
 
-map<pi, int> vis;
+int vis[31][31];
 int n;
 double p[4];
 int dx[4] = { -1, 1, 0, 0 }, dy[4] = { 0, 0, -1, 1 };
@@ -14,13 +14,13 @@ double recur(int now, int x, int y, int d) // 이미 now칸 이동했을 떄
 	if (now == n) {
 		return 1;
 	}
-	vis[{ x, y }]++;
+	vis[x][y] = 1;
 	double ret = 0;
 	for (int i = 0; i < 4; i++) {
-		if (vis.find({ x + dx[i], y + dy[i] }) != vis.end() && vis[{ x + dx[i], y + dy[i] }] > 0) continue;
+		if (vis[x + dx[i]][y + dy[i]]) continue;
 		ret += p[i] * recur(now + 1, x + dx[i], y + dy[i], i);
 	}
-	vis[{ x, y }]--;
+	vis[x][y] = 0;
 	return ret;
 }
 
@@ -31,7 +31,7 @@ void solve()
 		cin >> p[i];
 		p[i] /= 100;
 	}
-	cout << setprecision(12) << fixed << recur(0, 0, 0, 100);
+	cout << setprecision(12) << fixed << recur(0, 15, 15, 100);
 }
 
 signed main()
