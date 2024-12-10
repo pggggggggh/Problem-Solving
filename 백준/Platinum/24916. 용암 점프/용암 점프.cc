4e1aa068df6d2a39758f43b4252f9f7fe129a1de
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-12-10 21:22:10 +0900

    [Platinum III] Title: 용암 점프, Time: 916 ms, Memory: 2812 KB, Score: 67 point -BaekjoonHub

--- 백준/Platinum/24916. 용암 점프/용암 점프.cc
+++ 백준/Platinum/24916. 용암 점프/용암 점프.cc
@@ -1,8 +1,18 @@
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
 #define all(x) begin(x), end(x)
 #define int long long
 using namespace std;
 using pi = pair<int, int>;
+
+const int m = 1e3;
+
+inline int hsh(int x, int y, int z, int w)
+{
+	return x * m * m * m + y * m * m + z * m + w;
+}
 
 void solve()
 {
@@ -11,21 +21,24 @@ void solve()
 	vector<int> a(n + 2);
 	a[0] = 1e18;
 	a[n + 1] = -1e18;
-	vector<pi> b;
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
-		if (i > 1) b.push_back({ a[i] - a[i - 1], i });
 	}
 	if (n == 1) {
 		cout << "YES\n";
 		return;
 	}
-	map<array<int, 4>, int> dp;
+	if (n > 22) {
+		for (int i = 1; i <= n; i++) cout << "NO\n";
+		return;
+	}
+	unordered_map<int, int> dp;
 	function<int(int, int, int, int)> go = [&](int s, int e, int se, int lastcha) {
-		if (dp.find({ s, e, se, lastcha }) != dp.end()) {
-			return dp[{ s, e, se, lastcha }];
+		int jyheo = hsh(s, e, se, lastcha);
+		if (dp.find(jyheo) != dp.end()) {
+			return dp[jyheo];
 		}
-		int& ret = dp[{ s, e, se, lastcha }];
+		int& ret = dp[jyheo];
 		if (s == 1 && e == n) return ret = 1;
 		if (se == 0) {
 			if (a[s] - a[s - 1] >= 2 * lastcha) {
@@ -44,16 +57,7 @@ void solve()
 		}
 		return ret = 0;
 	};
-	sort(all(b));
-	set<int> candi;
-	for (int i = 1; i <= n; i++) candi.insert(i);
-	for (int i = 0; i < min((int)b.size(), 500LL); i++) {
-		int idx = b[i].second;
-		candi.insert(idx);
-		if (idx < n) candi.insert(idx + 1);
-		candi.insert(idx - 1);
-	}
-	for (auto& i : candi) {
+	for (int i = 1; i <= n; i++) {
 		dp.clear();
 		if (go(i, i, 0, 0)) cout << "YES\n";
 		else cout << "NO\n";
