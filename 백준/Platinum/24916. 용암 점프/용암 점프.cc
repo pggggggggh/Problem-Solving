331d881fab1ce9a6a371cebff247f90b65b0cb9b
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-12-10 20:44:09 +0900

    [Platinum III] Title: 용암 점프, Time: 0 ms, Memory: 2024 KB, Score: 13 point -BaekjoonHub

--- 백준/Platinum/24916. 용암 점프/용암 점프.cc
+++ 백준/Platinum/24916. 용암 점프/용암 점프.cc
@@ -8,70 +8,72 @@ void solve()
 {
 	int n;
 	cin >> n;
-	vector<int> a(n + 1);
-	vector<int> ans(n + 1);
-	ans[1] = ans[n] = 1;
+	vector<int> a(n + 2);
+	a[0] = 1e18;
+	a[n + 1] = -1e18;
 	for (int i = 1; i <= n; i++) cin >> a[i];
 	if (n == 1) {
 		cout << "YES\n";
 		return;
 	}
-	for (int i = 3; i <= n; i++) {
-		if (a[i] - a[i - 1] < 2 * (a[i - 1] - a[i - 2])) {
-			ans[1] = 0;
-			break;
-		}
-	}
-	for (int i = n - 2; i >= 1; i--) {
-		if (a[i + 1] - a[i] < 2 * (a[i + 2] - a[i + 1])) {
-			ans[n] = 0;
-			break;
-		}
-	}
-	vector<int> b(n + 1);
-	b[0] = b[n] = -1e18;
-	int s = -1;
-	for (int i = 1; i <= n - 1; i++) {
-		b[i] = a[i + 1] - a[i];
-		if (s == -1 || b[i] < b[s]) {
-			s = i;
-		}
-	}
-	int e = s;
-	int reals = s;
-	int cur = b[s];
-	int firstmove = 0;
-	while (1) {
-		// cout << s << ' ' << e << '\n';
-		if (s == 1 && e == n - 1) {
-			if (firstmove == -1) ans[reals] = 1;
-			else ans[reals + 1] = 1;
-			break;
-		}
-		if (b[s - 1] >= cur && b[e + 1] >= cur) {
-			if (b[s - 1] < b[e + 1]) {
-				if (firstmove == 0) firstmove = -1;
-				s--;
-				cur += b[s];
+	function<int(int)> go = [&](int s) {
+		int e = s;
+		int se = 0; // 0이면 s, 1이면 e
+		int cha = 0;
+		while (1) {
+			if (s == 1 && e == n) return 1;
+			if (se) {
+				if (a[e] - a[s - 1] >= 2 * cha && a[e + 1] - a[e] >= 2 * cha) {
+					if (rand() % 2) {
+						cha = a[e] - a[s - 1];
+						s--;
+						se = 0;
+					} else {
+						cha = a[e + 1] - a[e];
+						e++;
+						se = 1;
+					}
+				} else if (a[e] - a[s - 1] >= 2 * cha) {
+					cha = a[e] - a[s - 1];
+					s--;
+					se = 0;
+				} else if (a[e + 1] - a[e] >= 2 * cha) {
+					cha = a[e + 1] - a[e];
+					e++;
+					se = 1;
+				} else return 0;
 			} else {
-				if (firstmove == 0) firstmove = 1;
-				e++;
-				cur += b[e];
+				if (a[s] - a[s - 1] >= 2 * cha && a[e + 1] - a[s] >= 2 * cha) {
+					if (rand() % 2) {
+						cha = a[s] - a[s - 1];
+						s--;
+						se = 0;
+					} else {
+						cha = a[e + 1] - a[s];
+						e++;
+						se = 1;
+					}
+				} else if (a[s] - a[s - 1] >= 2 * cha) {
+					cha = a[s] - a[s - 1];
+					s--;
+					se = 0;
+				} else if (a[e + 1] - a[s] >= 2 * cha) {
+					cha = a[e + 1] - a[s];
+					e++;
+					se = 1;
+				} else return 0;
 			}
-		} else if (b[s - 1] >= cur) {
-			if (firstmove == 0) firstmove = -1;
-			s--;
-			cur += b[s];
-		} else if (b[e + 1] >= cur) {
-			if (firstmove == 0) firstmove = 1;
-			e++;
-			cur += b[e];
-		} else {
-			break;
 		}
-	}
+	};
 	for (int i = 1; i <= n; i++) {
-		if (ans[i]) cout << "YES\n";
+		int flag = 0;
+		for (int tc = 0; tc < 2000; tc++) {
+			if (go(i)) {
+				flag = 1;
+				break;
+			}
+		}
+		if (flag) cout << "YES\n";
 		else cout << "NO\n";
 	}
 }
