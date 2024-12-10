0168b0930e41d306a6f65cb9a137f549fececbaa
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2024-12-10 22:00:04 +0900

    [Platinum III] Title: 용암 점프, Time: 664 ms, Memory: 2604 KB, Score: 67 point -BaekjoonHub

--- 백준/Platinum/24916. 용암 점프/용암 점프.cc
+++ 백준/Platinum/24916. 용암 점프/용암 점프.cc
@@ -3,24 +3,27 @@
 #pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
 #define all(x) begin(x), end(x)
-#define int long long
+// #define int long long
 using namespace std;
 using pi = pair<int, int>;
 
 const int m = 1e3;
 
-inline int hsh(int x, int y, int z, int w)
+inline long long hsh(int x, int y, int z)
 {
-	return (x * m * m * m + y * m * m + z * m + w) % (int)1e12;
+	return (x * m * m + y * m + z);
 }
+
+int a[100005];
+unordered_map<long long, int> dp;
 
 void solve()
 {
 	int n;
 	cin >> n;
-	vector<int> a(n + 2);
-	a[0] = 1e18;
-	a[n + 1] = -1e18;
+	for (int i = 0; i <= n + 1; i++) a[i] = 0;
+	a[0] = 1e9;
+	a[n + 1] = -1e9;
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
 	}
@@ -32,34 +35,52 @@ void solve()
 		for (int i = 1; i <= n; i++) cout << "NO\n";
 		return;
 	}
-	unordered_map<int, int> dp;
-	function<int(int, int, int, int)> go = [&](int s, int e, int se, int lastcha) {
-		int jyheo = hsh(s, e, se, lastcha);
+
+	function<int(int, int, int)> go = [&](int s, int e, int sex) {
+		int jyheo = hsh(s, e, sex);
 		if (dp.find(jyheo) != dp.end()) {
 			return dp[jyheo];
 		}
 		int& ret = dp[jyheo];
 		if (s == 1 && e == n) return ret = 1;
+		int lastcha, se;
+		if (sex == 0) {
+			se = 0;
+			lastcha = a[s + 1] - a[s];
+		} else if (sex == 1) {
+			se = 1;
+			lastcha = a[e] - a[s];
+		} else if (sex == 2) {
+			se = 1;
+			lastcha = a[e] - a[e - 1];
+		} else {
+			se = 0;
+			lastcha = a[e] - a[s];
+		}
+		if (s == e) {
+			se = 0;
+			lastcha = 0;
+		}
 		if (se == 0) {
 			if (a[s] - a[s - 1] >= 2 * lastcha) {
-				if (go(s - 1, e, 0, a[s] - a[s - 1])) return ret = 1;
+				if (go(s - 1, e, 0)) return ret = 1;
 			}
 			if (a[e + 1] - a[s] >= 2 * lastcha) {
-				if (go(s, e + 1, 1, a[e + 1] - a[s])) return ret = 1;
+				if (go(s, e + 1, 1)) return ret = 1;
 			}
 		} else {
 			if (a[e + 1] - a[e] >= 2 * lastcha) {
-				if (go(s, e + 1, 1, a[e + 1] - a[e])) return ret = 1;
+				if (go(s, e + 1, 2)) return ret = 1;
 			}
 			if (a[e] - a[s - 1] >= 2 * lastcha) {
-				if (go(s - 1, e, 0, a[e] - a[s - 1])) return ret = 1;
+				if (go(s - 1, e, 3)) return ret = 1;
 			}
 		}
 		return ret = 0;
 	};
 	for (int i = 1; i <= n; i++) {
 		dp.clear();
-		if (go(i, i, 0, 0)) cout << "YES\n";
+		if (go(i, i, 0)) cout << "YES\n";
 		else cout << "NO\n";
 	}
 }
