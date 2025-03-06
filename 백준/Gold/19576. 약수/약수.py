70ae09e22f3436100c7d9ffe2603a4019d4ca9a9
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2025-03-06 17:42:10 +0900

    [Gold III] Title: 약수, Time: 320 ms, Memory: 110896 KB -BaekjoonHub

--- 백준/Gold/19576. 약수/약수.py
+++ 백준/Gold/19576. 약수/약수.py
@@ -1,12 +1,10 @@
 n = int(input())
-a = list(map(int, input().split()))
-a.sort()  # 오름차순 정렬
-
-dp = [1] * n  # dp[i]는 i번째 원소를 마지막으로 하는 최대 약수-배수 관계의 길이
-
-for i in range(n):
+a = [1, *list(map(int, input().split()))]
+a.sort()
+dp = [1 for _ in range(n + 1)]
+dp[0] = 0
+for i in range(1, n + 1):
     for j in range(i):
-        if a[i] % a[j] == 0:  # a[i]가 a[j]의 배수인 경우
+        if a[i] % a[j] == 0:
             dp[i] = max(dp[i], dp[j] + 1)
-
-print(n - max(dp))  # 전체 길이에서 가장 긴 약수-배수 관계의 길이를 뺀 값
+print(n - max(dp))
