1fc07ebf175a2b6b126aeddc5f6716c215890cf3
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2025-03-07 06:00:30 +0900

    [Platinum V] Title: Strongly Connected Component, Time: 20 ms, Memory: 4576 KB -BaekjoonHub

--- 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.cc
+++ 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.cc
@@ -1,50 +1,48 @@
 #include <bits/stdc++.h>
-#define all(v) (v).begin(), (v).end()
+#define all(x) begin(x), end(x)
+#define all1(x) begin(x) + 1, end(x)
 #define int long long
 using namespace std;
 using pi = pair<int, int>;
-
-vector<int> adj[10005];
-vector<int> id(10005), finished(10005);
-int dfn;
-vector<vector<int>> sccs;
-vector<int> stk;
-
-int go(int u)
-{
-	id[u] = ++dfn;
-	stk.push_back(u);
-	int top = id[u];
-	for (auto& v : adj[u]) {
-		if (id[v] == 0) top = min(top, go(v));
-		else if (!finished[v]) top = min(top, id[v]);
-	}
-	if (top == id[u]) {
-		vector<int> scc;
-		while (1) {
-			int tmp = stk.back();
-			finished[tmp] = 1;
-			scc.push_back(tmp);
-			stk.pop_back();
-			if (tmp == u) break;
-		}
-		sort(all(scc));
-		sccs.push_back(scc);
-	}
-	return top;
-}
 
 void solve()
 {
 	int n, m;
 	cin >> n >> m;
+	vector<vector<int>> adj(n + 1);
 	for (int i = 0; i < m; i++) {
 		int x, y;
 		cin >> x >> y;
 		adj[x].push_back(y);
 	}
+	vector<vector<int>> sccs;
+	vector<int> id(n + 1), low(n + 1), fin(n + 1);
+	vector<int> stk;
+	int dfn = 0;
+	function<int(int)> dfs = [&](int u) {
+		stk.push_back(u);
+		id[u] = ++dfn;
+		low[u] = dfn;
+		for (auto& v : adj[u]) {
+			if (!low[v]) low[u] = min(low[u], dfs(v));
+			else if (!fin[v]) low[u] = min(low[u], low[v]);
+		}
+		if (low[u] == id[u]) {
+			vector<int> scc;
+			while (1) {
+				int tmp = stk.back();
+				fin[tmp] = 1;
+				scc.push_back(tmp);
+				stk.pop_back();
+				if (tmp == u) break;
+			}
+			sort(all(scc));
+			sccs.push_back(scc);
+		}
+		return low[u];
+	};
 	for (int i = 1; i <= n; i++)
-		if (!id[i]) go(i);
+		if (!low[i]) dfs(i);
 	sort(all(sccs));
 	cout << sccs.size() << '\n';
 	for (auto& scc : sccs) {
@@ -57,7 +55,6 @@ signed main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	cout.tie(0);
 
 	solve();
 }
