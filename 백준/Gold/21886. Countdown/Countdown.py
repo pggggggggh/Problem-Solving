9629d5898cc5bc3a957e052acd59b0b3206257bf
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2025-04-15 14:23:34 +0900

    [Gold III] Title: Countdown, Time: 140 ms, Memory: 143760 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21886. Countdown/Countdown.py
+++ 백준/Gold/21886. Countdown/Countdown.py
@@ -1,17 +1,12 @@
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
 psum = 0
-for i in range(n):
+for i in range(n - 1):
     psum += a[i]
-    if i == 0:
-        gt = -1
-    else:
-        gt = a[i - 1]
-    leq = a[i]
-    if (k + psum) % (i + 1) != 0:
-        continue
-    ans = (k + psum) // (i + 1)
-    if ans > gt and ans <= leq:
-        print(ans)
-        exit(0)
-print((k + psum) // n)
+    if (k + psum) % (i + 1) == 0:
+        ans = (k + psum) // (i + 1)
+        if ans > a[i] and ans <= a[i + 1]:
+            print(ans)
+            exit(0)
+psum += a[n - 1]
+print((psum + k) // n)
