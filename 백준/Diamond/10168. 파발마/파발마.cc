01a964cae516b42ea0d1ccef0ce2e1b482252ae2
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2025-05-27 13:05:29 +0900

    [Diamond V] Title: 파발마, Time: 116 ms, Memory: 22776 KB, Score: 100 point -BaekjoonHub

--- 백준/Diamond/10168. 파발마/파발마.cc
+++ 백준/Diamond/10168. 파발마/파발마.cc
@@ -5,6 +5,11 @@
 using namespace std;
 using pi = pair<int, int>;
 
+double intersect(pi a, pi b)
+{
+	return (double)(b.second - a.second) / (double)(a.first - b.first);
+}
+
 void solve()
 {
 	int n;
@@ -17,19 +22,43 @@ void solve()
 	}
 	int m = a.size() - 1;
 	vector<int> ldp(m + 2, 1e18), rdp(m + 2, 1e18);
-	ldp[0] = rdp[m + 1] = 0;
+	ldp[0] = 0;
+	vector<pair<double, pi>> lines;
+	lines.push_back({ 0, { 0, 0 } });
 	for (int i = 1; i <= m; i++) {
-		for (int j = 0; j < i; j++) {
-			ldp[i] = min(ldp[i], ldp[j] + a[i] * (i - j + 1));
+		auto it = prev(upper_bound(all(lines), make_pair((double)a[i], (pi)make_pair(1e18, 1e18))));
+		ldp[i] = a[i] * (i + 1) - it->second.first * a[i] - it->second.second;
+
+		pi cur = { i, -ldp[i] };
+		while (lines.size() >= 2 && intersect((prev(prev(lines.end()))->second), lines.back().second) >= intersect(lines.back().second, cur)) {
+			lines.pop_back();
 		}
+		lines.push_back({ intersect(lines.back().second, cur), cur });
 	}
-	for (int i = m; i >= 1; i--) {
-		for (int j = i + 1; j <= m + 1; j++) {
-			rdp[i] = min(rdp[i], rdp[j] + (n - a[i] + 1) * (j - i + 1));
+
+	vector<int> b(1);
+	for (int i = 1; i <= m; i++) b.push_back(n + 1 - a[i]);
+	b.push_back(0);
+	reverse(all(b));
+	b.pop_back();
+	a = b;
+	lines.clear();
+	rdp[0] = 0;
+	lines.push_back({ 0, { 0, 0 } });
+	for (int i = 1; i <= m; i++) {
+		auto it = prev(upper_bound(all(lines), make_pair((double)a[i], (pi)make_pair(1e18, 1e18))));
+		rdp[i] = a[i] * (i + 1) - it->second.first * a[i] - it->second.second;
+
+		pi cur = { i, -rdp[i] };
+		while (lines.size() >= 2 && intersect((prev(prev(lines.end()))->second), lines.back().second) >= intersect(lines.back().second, cur)) {
+			lines.pop_back();
 		}
+		lines.push_back({ intersect(lines.back().second, cur), cur });
 	}
+	vector<int> realrdp(m + 2);
+	for (int i = 1; i <= m; i++) realrdp[i] = rdp[m + 1 - i];
 	int ans = 1e18;
-	for (int i = 0; i <= m; i++) ans = min(ans, ldp[i] + rdp[i + 1]);
+	for (int i = 0; i <= m; i++) ans = min(ans, ldp[i] + realrdp[i + 1]);
 	cout << ans;
 }
 
