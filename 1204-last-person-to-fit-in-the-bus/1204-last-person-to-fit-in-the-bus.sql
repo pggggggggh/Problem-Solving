460c8234c8484b164aa5c1749f22fb29cd6c09a1
pggggggggh <129352573+pggggggggh@users.noreply.github.com>
2026-08-26 03:30:22 +0900

    Time: 859 ms (66.87%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,10 +1,7 @@
 # Write your MySQL query statement below
-SELECT t2.person_name
-FROM (
-    SELECT LAG(t.acc_sum,1) OVER (ORDER BY turn) prev_acc_sum, t.acc_sum, t.person_name, t.turn FROM (
-        SELECT SUM(weight) OVER (ORDER BY turn) acc_sum, turn, person_name FROM Queue ORDER BY turn
-    ) t
-) t2
-WHERE t2.acc_sum <= 1000
-ORDER BY t2.turn DESC
+SELECT t.person_name FROM (
+    SELECT SUM(weight) OVER (ORDER BY turn) acc_sum, turn, person_name FROM Queue ORDER BY turn
+) t
+WHERE t.acc_sum <= 1000
+ORDER BY t.turn DESC
 LIMIT 1;
